2d232ac1e71bd88f5031eac75dd434c9b5a73e2f
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-05-11 19:09:15 +0200

    added a comment

--- GitHub/Program.cs
+++ GitHub/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//this comment is an example. after this go to git changes next to solution explorer. then add a comment, commit and push
